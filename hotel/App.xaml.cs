8f6773de1d2c7008efb9b1bbfb145b24cbfbc96d
João Victor Bento <jhonnesbdd@gmail.com>
2025-08-22 04:16:40 -0300

    filtros de segurança na date, e models

--- hotel/App.xaml.cs
+++ hotel/App.xaml.cs
@@ -1,7 +1,51 @@
-﻿namespace hotel
+﻿using hotel.Models;
+
+namespace hotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+
+            new Quarto()
+            {
+
+                Descricao = "Suite super luxo",
+                ValordadiariaAdulto = 110.00,
+                ValordadiariaCrianca = 55.00
+
+            },
+
+            new Quarto()
+            {
+
+                Descricao = "Suite luxo",
+                ValordadiariaAdulto = 80.00,
+                ValordadiariaCrianca = 40.00
+
+            },
+
+            new Quarto()
+            {
+
+                Descricao = "Suite single",
+                ValordadiariaAdulto = 50.00,
+                ValordadiariaCrianca = 25.00
+
+            },
+
+            new Quarto()
+            {
+
+                Descricao = "Suite Crise",
+                ValordadiariaAdulto = 25.00,
+                ValordadiariaCrianca = 12.50
+
+            }
+
+        };
+
         public App()
         {
             
--- hotel/interface_grafica/HospedagemContratada.xaml.cs
+++ hotel/interface_grafica/HospedagemContratada.xaml.cs
@@ -6,4 +6,22 @@ public partial class HospedagemContratada : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+		try
+		{
+
+			Navigation.PopAsync();
+
+		}
+		catch(Exception ex)
+		{
+
+			DisplayAlert("OPS", ex.Message, "Ok");
+
+		}
+
+    }
 }
